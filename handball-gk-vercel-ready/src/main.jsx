018c1e9494ee58f6b1c46b771793e9d5f256d013
Mariano Losada <89931647+marianodip12@users.noreply.github.com>
2026-03-04 15:29:24 -0300

    Delete handball-gk-vercel-ready directory

--- handball-gk-vercel-ready/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import HandballGKApp from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HandballGKApp />
-  </React.StrictMode>
-)
